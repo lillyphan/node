1531d0e7deca30bccb85900efa6591c4fb9be373
lillyphan <lphan24@kentdenver.org>
2022-09-26 12:00:35 -0600

    Replace int type data with generic type <E>

--- src/Node.java
+++ src/Node.java
@@ -1,60 +1,58 @@
 import java.util.Objects;
 
-public class Node {
-    private Node parent;
-    private Node child;
-    private int data; //change this obviously
+public class Node<E> {
+    private Node<E> parent;
+    private Node<E> child;
+    private E data;
 
     public Node(){
     }
 
-    public Node(int data){
+    public Node(E data){
         this.data = data;
     }
 
-    public Node(int data, Node child, Node parent){
+    public Node(E data, Node<E> child, Node<E> parent){
         this.data = data;
         this.parent = parent;
         this.child = child;
     }
 
     //accessors
-    public int getData() {
+    public E getData() {
         return data;
     }
 
-    public Node getChild() {
+    public Node<E> getChild() {
         return child;
     }
 
-    public Node getParent() {
+    public Node<E> getParent() {
         return parent;
     }
 
     //mutators
-    public void setChild(Node child) {
+    public void setChild(Node<E> child) {
         this.child = child;
     }
 
-    public void setData(int data) {
+    public void setData(E data) {
         this.data = data;
     }
 
-    public void setParent(Node parent) {
+    public void setParent(Node<E> parent) {
         this.parent = parent;
     }
 
     //other
-
 
     @Override
     public String toString() {
         return "Node: " + data;
     }
 
-    @Override
-    public boolean equals(Node n) {
-        return this.data == n.data;
+    public boolean equals(Node<E> node) {
+        return Objects.equals(data, node.data);
     }
 
 }
