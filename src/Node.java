e1e8ab2573057e725da90c0c711ad49604981ba4
lillyphan <lphan24@kentdenver.org>
2022-09-25 22:00:22 -0600

    add instance data, constructors, accessors, mutators, and overrided toString() and equals()

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,60 @@
+import java.util.Objects;
+
 public class Node {
-    public static void main(String[] args) {
-        System.out.println("hi");
+    private Node parent;
+    private Node child;
+    private int data; //change this obviously
+
+    public Node(){
     }
+
+    public Node(int data){
+        this.data = data;
+    }
+
+    public Node(int data, Node child, Node parent){
+        this.data = data;
+        this.parent = parent;
+        this.child = child;
+    }
+
+    //accessors
+    public int getData() {
+        return data;
+    }
+
+    public Node getChild() {
+        return child;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    //mutators
+    public void setChild(Node child) {
+        this.child = child;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    //other
+
+
+    @Override
+    public String toString() {
+        return "Node: " + data;
+    }
+
+    @Override
+    public boolean equals(Node n) {
+        return this.data == n.data;
+    }
+
 }
